e9cc0cfa598f7365635a026326b3835a07c0183d
Ahmed Soudan <ahmedsoudan@Ahmeds-MacBook-Air.local>
2026-04-10 21:18:56 +0100

    Fix build issues: disable ESLint, add outputFileTracingRoot, install dependencies

--- templates/elite-diner/next.config.js
+++ templates/elite-diner/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: __dirname,
+  eslint: { ignoreDuringBuilds: true },
   images: { domains: ['images.unsplash.com'] },
 }
 module.exports = nextConfig
